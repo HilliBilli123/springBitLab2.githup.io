0818002d8dc9e0384c3f712219a44094b54afc37
HilliBilli <qwe.mister.qwe@mail.ru>
2024-12-16 18:57:38 +0500

    123

--- js/script.js
+++ js/script.js
@@ -2,10 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
   let accessKey = sessionStorage.getItem("accessKey");
   if (
     !accessKey &&
-    (window.location.pathname === "/index.html" ||
-      window.location.pathname === "/pages/ExchangeRates.html")
+    (window.location.pathname === "/springBitLab2.githup.io/index.html" ||
+      window.location.pathname ===
+        "/springBitLab2.githup.io/pages/ExchangeRates.html")
   ) {
-    window.location.href = window.location.origin + "/pages/auth.html";
+    window.location.href =
+      window.location.origin + "/springBitLab2.githup.io/pages/auth.html";
   }
 });
 function setSession() {
@@ -16,7 +18,8 @@ function setSession() {
   parsetJson.forEach((element) => {
     if (element.login == login.value && element.password == password.value) {
       sessionStorage.setItem("accessKey", Math.random());
-      window.location.href = window.location.origin + "/index.html";
+      window.location.href =
+        window.location.origin + "/springBitLab2.githup.io/index.html";
     }
   });
 }
@@ -54,13 +57,16 @@ function setLocalStoreg() {
     parsetJson.push(person);
     localStorage.setItem("persons", JSON.stringify(parsetJson));
   }
-  window.location.href = window.location.origin + "/pages/auth.html";
+  window.location.href =
+    window.location.origin + "/springBitLab2.githup.io/pages/auth.html";
 }
 function regisPage() {
-  window.location.href = window.location.origin + "/pages/regis.html";
+  window.location.href =
+    window.location.origin + "/springBitLab2.githup.io/pages/regis.html";
 }
 function authPage() {
-  window.location.href = window.location.origin + "/pages/auth.html";
+  window.location.href =
+    window.location.origin + "/springBitLab2.githup.io/pages/auth.html";
 }
 function clearSession() {
   window.sessionStorage.clear();
